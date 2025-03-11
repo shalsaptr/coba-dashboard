15a83a73a528073ab1ae38a48b5b51ff8299cd68
shalsaptr <shalsaputriiii@gmail.com>
2025-03-11 10:35:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Hitung Luas Persegi Panjang")
 
-panjang = st.number_input ("Masukkan nilai panjang", 0)
-lebar = st.number_input ("Masukkan nilai lebar", 0)
+panjang = st.number_input ("nilai panjang", 0)
+lebar = st.number_input ("nilai lebar", 0)
 hitung = st.button ("Hitung")
