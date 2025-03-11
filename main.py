445ceff0bdd6d31b29170fb567dab87aee48f60c
shalsaptr <shalsaputriiii@gmail.com>
2025-03-11 10:34:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ st.title("Hitung Luas Persegi Panjang")
 
 panjang = st.number_input ("Masukkan nilai panjang", 0)
 lebar = st.number_input ("Masukkan nilai lebar", 0)
-hitung = st.button ("Hitung luas")
+hitung = st.button ("Hitung")
